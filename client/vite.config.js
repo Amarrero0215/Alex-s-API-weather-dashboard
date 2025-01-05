51b48fb0170131da86ee786004f61a353370f504
Alex Marrero <alexmarrero0215@gmail.com>
2025-01-05 18:42:56 -0500

    Fixed potential deployment issue with vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Alex-s-API-weather-dashboard',
+  build: {
+    outDir: 'Dist', // Ensures output folder is 'dist'
+  },
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
 });
