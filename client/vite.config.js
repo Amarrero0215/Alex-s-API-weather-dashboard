97263ec2bc4f9fcd00e4d5dbb176562ce849a261
Alex Marrero <alexmarrero0215@gmail.com>
2025-01-05 18:40:26 -0500

    fixing small update on branches

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-/* export default defineConfig({
+export default defineConfig({
   server: {
     port: 3000,
     open: true,
@@ -12,16 +12,5 @@ import { defineConfig } from 'vite';
         secure: false,
       },
     },
-  },
-}); */
-
-export default defineConfig({
-  base: 'Alex-s-API-weather-dashboard',
-  build: {
-    outDir: 'dist', // Ensures output folder is 'dist'
-  },
-  server: {
-    port: 3000,
-    open: true,
   },
 });
