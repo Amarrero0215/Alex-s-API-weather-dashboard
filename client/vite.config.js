d95cde3f290104664baaba608c6c4020520363df
Alex Marrero <alexmarrero0215@gmail.com>
2025-01-05 19:02:07 -0500

    Undoed deployment changes in config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Alex-s-API-weather-dashboard',
-  build: {
-    outDir: 'Dist', // Ensures output folder is 'dist'
-  },
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
